6157c17c5decb02ba311e8031edf7805870f6504
panyu <344146473@qq.com>
2020-06-02 00:09:55 +0800

    SequenceInputStreamDemo

---
+++ src/main/java/com/panyu/jase/iodemo/sequenceinputstream/SequenceInputStreamDemo.java
@@ -0,0 +1,27 @@
+package com.panyu.jase.iodemo.sequenceinputstream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+
+public class SequenceInputStream {
+
+    public static void main(String[] args) throws IOException {
+
+        // 演示序列流 SequenceInputStream
+        // 如何获取一个Enumeration呢？Vector有，效率低，使用ArrayList
+        ArrayList<FileInputStream> al = new ArrayList<>();
+        // 添加三个输入流对象，和指定的文件关联
+        for (int i = 0; i < 3; i++) {
+            al.add(new FileInputStream("/Users/panyu/java_projects/gradledemo/src/main/java/com/panyu/jase/iodemo/sequenceinputstream/1.txt"));
+        }
+
+        // 怎么通过ArrayList获取枚举接口。可以使用Collections工具类中的方法
+        Enumeration<FileInputStream> en = Collections.enumeration(al);
+        // 创建序列流对象，需要传递Enumeration。
+        SequenceInputStream sis = new SequenceInputStream(en);
+
+    }
+}
